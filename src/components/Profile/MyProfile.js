6dfedc6861d74782d862d2f5acb5f65dc186c42b
Nina van den Braak <ninavandenbraak@gmail.com>
2020-12-13 23:01:37 +0100

    one extra change

--- src/components/Profile/MyProfile.js
+++ src/components/Profile/MyProfile.js
@@ -40,62 +40,64 @@ function MyProfile(props) {
               </Link>
             </div>
           </div>
-          <h5>My Christmas Tips</h5>
-          <div className='profileSection'>
-            {props.loggedInUser.tips.length <= 0 && (
-              <div>
-                <p>No tips added yet</p>
-                <Link to='/tips/add'>
-                  <button
-                    className='btn btn-warning'
-                    title='Go to addition form'>
-                    Add a Tip
-                  </button>
-                </Link>
-              </div>
-            )}
-            {props.loggedInUser.tips &&
-              props.loggedInUser.tips.map((item) => (
-                <TipsListItem item={item} key={item._id} />
-              ))}
-          </div>
-          <h5>My Favorites</h5>
-          <div className='profileSection'>
-            {props.loggedInUser.favorites.length <= 0 && (
-              <div>
-                <p>No favorites yet</p>
-                <Link to='/tips'>
-                  <button className='btn btn-warning' title='Go to Tips'>
-                    View Articles
-                  </button>
-                </Link>
-              </div>
-            )}
-            {props.loggedInUser.favorites &&
-              props.loggedInUser.favorites.map((item) => (
-                <FavoriteListItem item={item} key={item._id} />
-              ))}
-          </div>
-          <h5>Following</h5>
-          <div className='profileSection'>
-            {props.loggedInUser.following.length <= 0 && (
-              <div>
-                <p>You're not following anyone yet</p>
-              </div>
-            )}
-            {props.loggedInUser.following &&
-              props.loggedInUser.following.map((item) => (
-                <div className='listItem' key={item._id}>
-                  <Link to={`/profile/${item._id}`}>
-                    <article className='oneListItem followingListItem'>
-                      <img src={item.picture} alt={item.firstName} />
-                      <h4>
-                        {item.firstName} {item.lastName}
-                      </h4>
-                    </article>
+          <div className='profileContent'>
+            <h5>My Christmas Tips</h5>
+            <div className='profileSection'>
+              {props.loggedInUser.tips.length <= 0 && (
+                <div>
+                  <p>No tips added yet</p>
+                  <Link to='/tips/add'>
+                    <button
+                      className='btn btn-warning'
+                      title='Go to addition form'>
+                      Add a Tip
+                    </button>
                   </Link>
                 </div>
-              ))}
+              )}
+              {props.loggedInUser.tips &&
+                props.loggedInUser.tips.map((item) => (
+                  <TipsListItem item={item} key={item._id} />
+                ))}
+            </div>
+            <h5>My Favorites</h5>
+            <div className='profileSection'>
+              {props.loggedInUser.favorites.length <= 0 && (
+                <div>
+                  <p>No favorites yet</p>
+                  <Link to='/tips'>
+                    <button className='btn btn-warning' title='Go to Tips'>
+                      View Articles
+                    </button>
+                  </Link>
+                </div>
+              )}
+              {props.loggedInUser.favorites &&
+                props.loggedInUser.favorites.map((item) => (
+                  <FavoriteListItem item={item} key={item._id} />
+                ))}
+            </div>
+            <h5>Following</h5>
+            <div className='profileSection'>
+              {props.loggedInUser.following.length <= 0 && (
+                <div>
+                  <p>You're not following anyone yet</p>
+                </div>
+              )}
+              {props.loggedInUser.following &&
+                props.loggedInUser.following.map((item) => (
+                  <div className='listItem' key={item._id}>
+                    <Link to={`/profile/${item._id}`}>
+                      <article className='oneListItem followingListItem'>
+                        <img src={item.picture} alt={item.firstName} />
+                        <h4>
+                          {item.firstName} {item.lastName}
+                        </h4>
+                      </article>
+                    </Link>
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       ) : (
